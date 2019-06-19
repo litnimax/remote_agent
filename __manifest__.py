3cecbe4ea4a668073fb91d8ed852f39ac69e721a
Maxim Litnitskiy <litnimaxster@gmail.com>
2019-06-19 15:54:58 +0300

    10.0 port

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 {
     'name': 'Odoo remote agent',
-    'version': '11.0.1.0',
+    'version': '10.0.1.0',
     'author': 'Odooist',
     'maintainer': 'Odooist',
     'support': 'odooist@gmail.com',
